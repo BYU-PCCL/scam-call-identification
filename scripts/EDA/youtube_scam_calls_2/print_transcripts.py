ebf6609281bb7180678bac9d99d274673d3a696e
Conner Drake <connerdrake9@gmail.com>
2025-08-15 18:23:18 -0400

    add EDA - yt scam calls 2

---
+++ scripts/EDA/youtube_scam_calls_2/print_transcripts.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+if __name__ == "__main__":
+    df = pd.read_csv("src/ml_scam_detector/data/youtube_scam_calls_2/extracted_features/transcripts.csv")
+
+    print()
+    print("_____________________________________________________________________________")
+    print("|                                                                           |")
+    print("|  Printing examples of transcripts from dataset: \"Youtube Scam Calls 2\"    |")
+    print("|___________________________________________________________________________|")
+    print()
+
+    for i in range(3):
+        print()
+        print(f"- - - TRANSCRIPT EXAMPLE {i + 1}: - - -")
+        print()
+        print(df["dialogue"][i][:300] + "...")
+        print("      ... (TRUNCATED)")
+        print()
+        print()
+
+    print(f"Total transcripts: {len(df)}")
