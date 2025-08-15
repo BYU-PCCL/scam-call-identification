2e46e8eaa9058339ed3f4d5bc49e98446f8f6683
Conner Drake <connerdrake9@gmail.com>
2025-08-15 18:52:21 -0400

    add dwnld data from links script

---
+++ scripts/ETL/download_files_from_urls_in_txt_file.py
@@ -0,0 +1,31 @@
+import requests
+import sys
+import os
+import re
+from pathlib import Path
+
+from src.ml_scam_detector.utils.file_models import (
+    download_file_from_url,
+    get_urls_from_text_file
+)
+
+if __name__ == "__main__":
+    args = sys.argv
+    print(args)
+    # TODO - finish implementing args, using hard-coded values for now
+
+    urls = get_urls_from_text_file("src/ml_scam_detector/data/candor/file_urls.txt")
+
+    for i, url in enumerate(urls):
+        if i <= 60:
+            continue
+        print(f"\n\n -> Downloading from URL: {i + 1}/{len(urls)} \n")
+        print(url)
+        print()
+        download_file_from_url(url, 'D:\scam-detector\src\ml_scam_detector\data\candor')
+
+"""
+USAGE EXAMPLE/TEMPLATE
+
+
+"""
